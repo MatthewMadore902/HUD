3c11b1bba7158c9d3563454a7cd97ba712c7eea6
MatthewMadore902 <90638212+MatthewMadore902@users.noreply.github.com>
2021-09-22 15:30:54 -0300

    Fixed the multiplier
    fixed the multiplier and added more text.

--- Program.cs
+++ Program.cs
@@ -12,8 +12,8 @@ namespace HUD
 		{
 			//---------------Score------------------------
 
-			int score;
-			score = 0;
+			int ponitsEarnd;
+			ponitsEarnd = 0;
 			//Console.WriteLine("your curent score: " + score);
 
 			//Console.WriteLine("You have elimainated an enemy: + 50 point" );
@@ -48,8 +48,8 @@ namespace HUD
 			int multiplier;
 			multiplier = 2;
 
-			int FinelScore;
-			FinelScore = score * multiplier;
+			int score;
+			score = 0;
 
 
 
@@ -62,33 +62,37 @@ namespace HUD
 
 
 			//------------------All Of It Together!!!!!!!!!--------------
-			 Console.WriteLine("Your Current score: " + FinelScore + "            " + "Shield: " + shield + "            " + gameName);
+			Console.WriteLine("------------------------------------------------------------------------------------------------------------------------");
+			Console.WriteLine("Your Current score: " + score + "            " + "Shield: " + shield + "            " + gameName);
 
-			Console.WriteLine(" ");
-			//Console.WriteLine(multiplier);
-			Console.WriteLine(" ");
-
+			Console.WriteLine("Multiplier: " + multiplier);
+			
 			Console.WriteLine("Your Current Health: " + health + "            " + "Lives: " + lives + "                  " + "AMMO - " + ammo);
-		
+			Console.WriteLine("------------------------------------------------------------------------------------------------------------------------");
 			//----------Action-------------
 
+			Console.WriteLine("");
 			Console.WriteLine("Press any key to battle!!!");
 			Console.ReadKey();
 
-			Console.WriteLine("A ghoul came out of the bushes you killed it but you took some damage");
+			Console.WriteLine("");
+			Console.WriteLine("A ghoul came out of the bushes you killed it but you took some damage, You took 24 damage, lost 3 shield, used 12 ammo and gain 100 points!!!!!!!!!!!");
+			Console.WriteLine("");
 			health = 100.0f - 24.0f;
-			score = score + 100;
+			ponitsEarnd += 100;
+			score = score + ponitsEarnd * multiplier;
 			ammo = 64 - 12;
+			shield = "^^";
+
 
 			Console.ReadKey();
+			Console.WriteLine("------------------------------------------------------------------------------------------------------------------------");
+			Console.WriteLine("Your Current score: " + score + "            " + "Shield: " + shield + "            " + gameName);
 
-			Console.WriteLine("Your Current score: " + FinelScore + "            " + "Shield: " + shield + "            " + gameName);
-
-			Console.WriteLine(" ");
-			//Console.WriteLine(multiplier);
-			Console.WriteLine(" ");
+			Console.WriteLine("Multiplier: " + multiplier);
 
 			Console.WriteLine("Your Current Health: " + health + "            " + "Lives: " + lives + "                  " + "AMMO - " + ammo);
+			Console.WriteLine("------------------------------------------------------------------------------------------------------------------------");
 
 			Console.ReadKey();
 			Console.ReadKey();
