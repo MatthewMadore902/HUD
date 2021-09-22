b29eab647ec551e29e95225bee66a7b901a47224
MatthewMadore902 <90638212+MatthewMadore902@users.noreply.github.com>
2021-09-22 15:00:47 -0300

    Working on multiplier

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ namespace HUD
 			//Console.WriteLine("your curent score: " + score);
 
 			//Console.WriteLine("You have elimainated an enemy: + 50 point" );
-			score = 0 + 50;
+			//score = 0 + 50;
 			//Console.WriteLine("Your current score is: " + score);
 
 			//----------Shield-------------
@@ -44,6 +44,15 @@ namespace HUD
 			gameName = "Best Game Ever";
 			//Console.WriteLine(gameName);
 
+			//-------------Multiplier-------------
+			int multiplier;
+			multiplier = 2;
+
+			int FinelScore;
+			FinelScore = score * multiplier;
+
+
+
 
 			//----------Ammo-------------
 			int ammo;
@@ -53,77 +62,36 @@ namespace HUD
 
 
 			//------------------All Of It Together!!!!!!!!!--------------
-
-			Console.WriteLine("Your Current score: " + score + "            " + "Shield: " + shield + "            " + gameName);
-
-			Console.WriteLine(" ");
+			 Console.WriteLine("Your Current score: " + FinelScore + "            " + "Shield: " + shield + "            " + gameName);
 
 			Console.WriteLine(" ");
-
-			Console.WriteLine("Your Current Health: " + health + "            " + "Lives: " + lives + "                  " + "AMMO - " + ammo);
-
-
-			//---------------Action Happaning----------------------
-
-			Console.WriteLine(" ");
-			Console.WriteLine(" ");
-			Console.WriteLine("You were attacked by an enemy!!!");
-			Console.WriteLine("The enemy dropped shield");
-			Console.WriteLine(" ");
-			Console.WriteLine(" ");
-			score = 0 + 50;
-			health = 95.0f;
-			shield = "^^";
-			ammo = 54;
-
-
-			Console.WriteLine("Your Current score: " + score + "            " + "Shield: " + shield + "            " + gameName);
-
-			Console.WriteLine(" ");
-
-			Console.WriteLine(" ");
-
+			//Console.WriteLine(multiplier);
 			Console.WriteLine(" ");
 
 			Console.WriteLine("Your Current Health: " + health + "            " + "Lives: " + lives + "                  " + "AMMO - " + ammo);
+		
+			//----------Action-------------
 
-			//Console.ReadLine
-
-			Console.WriteLine(" ");
-			Console.WriteLine(" ");
-			Console.WriteLine("You shoot an enemy in the distance you take him out");
-			Console.WriteLine(" ");
-			Console.WriteLine(" ");
-
-			ammo = 30;
-			score = score + 100;
-
-			Console.WriteLine("Your Current score: " + score + "            " + "Shield: " + shield + "            " + gameName);
-
-			Console.WriteLine(" ");
-
-			Console.WriteLine(" ");
-
-			Console.WriteLine(" ");
-
-			Console.WriteLine("Your Current Health: " + health + "            " + "Lives: " + lives + "                  " + "AMMO - " + ammo);
-
-			Console.WriteLine(" ");
-			Console.WriteLine(" ");
-			Console.WriteLine("");
-			Console.WriteLine(" ");
-			Console.WriteLine(" ");
-			
-			/*int kill;
-
-			if string kill ("kill an enemy");
-			{
-				score = score + 100;			
-			}*/
-
-			
+			Console.WriteLine("Press any key to battle!!!");
 			Console.ReadKey();
 
+			Console.WriteLine("A ghoul came out of the bushes you killed it but you took some damage");
+			health = 100.0f - 24.0f;
+			score = score + 100;
+			ammo = 64 - 12;
+
+			Console.ReadKey();
+
+			Console.WriteLine("Your Current score: " + FinelScore + "            " + "Shield: " + shield + "            " + gameName);
+
+			Console.WriteLine(" ");
+			//Console.WriteLine(multiplier);
+			Console.WriteLine(" ");
+
+			Console.WriteLine("Your Current Health: " + health + "            " + "Lives: " + lives + "                  " + "AMMO - " + ammo);
+
+			Console.ReadKey();
+			Console.ReadKey();
 		}
 
 			
